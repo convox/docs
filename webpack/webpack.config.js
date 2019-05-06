f8437590131af07d344676abdb88ffeb4cd2b9e4
David Dollar <david@dollar.io>
2019-05-06 16:01:52 -0400

    change to dart sass to work around sass/node-sass#2625

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -37,7 +37,12 @@ module.exports = {
               }
             }
           },
-          { loader: 'sass-loader' }
+          {
+            loader: 'sass-loader',
+            options: {
+              implementation: require('sass')
+            }
+          }
         ]
       },
       {
